f7c31150c7ec0909c0154135e3d06a2013aaf088
Rochelle <rochelle.rebucas@gmail.com>
2018-12-11 09:14:44 -0800

    portfolio update

--- javascript/home.js
+++ javascript/home.js
@@ -5,36 +5,3 @@ var onHover = function() {
 var offHover = function() {
     $("#footerImg").attr('src', 'images/face1.png');
 };
-
-
-
-var animatePoints = function() {
-
-  var revealPoint = function() {
-
-      $(this).css({
-          opacity: 1,
-          transform: 'scaleX(1) translateY(0)'
-      });
-
-     };
-
-     $.each($('.skill'), revealPoint);
-
- };
-
- $(window).load(function() {
-
-   if ($(window).height() > 950) {
-         animatePoints();
-     }
-
-   var scrollDistance = $('.skills').offset().top - $(window).height() + 200;
-
-   $(window).scroll(function(event) {
-     if ($(window).scrollTop() >= scrollDistance) {
-            animatePoints();
-          }
-    });
-
-});
